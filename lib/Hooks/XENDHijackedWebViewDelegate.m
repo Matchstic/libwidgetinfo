b0f5eb4d7464aa59f29017ea32d4a698a0370d2e
Matt Clarke <mclarke0805@gmail.com>
2020-03-24 19:37:28 +0000

    Add missing WKNavigationDelegate forwarding

--- lib/Hooks/XENDHijackedWebViewDelegate.m
+++ lib/Hooks/XENDHijackedWebViewDelegate.m
@@ -30,22 +30,31 @@
 // WKNavigationDelegate
 ///////////////////////////////////////////////////////
 
-- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+- (void)webView:(WKWebView *)webView
+    didStartProvisionalNavigation:(WKNavigation *)navigation {
+    
     if ([self.originalDelegate respondsToSelector:@selector(webView:didStartProvisionalNavigation:)])
         [self.originalDelegate webView:webView didStartProvisionalNavigation:navigation];
 }
 
-- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+- (void)webView:(WKWebView *)webView
+    didFailProvisionalNavigation:(WKNavigation *)navigation
+    withError:(NSError *)error {
+    
     if ([self.originalDelegate respondsToSelector:@selector(webView:didFailProvisionalNavigation:withError:)])
         [self.originalDelegate webView:webView didFailProvisionalNavigation:navigation withError:error];
 }
 
-- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+- (void)webView:(WKWebView *)webView
+    didCommitNavigation:(WKNavigation *)navigation {
+    
     if ([self.originalDelegate respondsToSelector:@selector(webView:didCommitNavigation:)])
         [self.originalDelegate webView:webView didCommitNavigation:navigation];
 }
 
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+- (void)webView:(WKWebView *)webView
+    didFinishNavigation:(WKNavigation *)navigation {
+    
     if ([self.originalDelegate respondsToSelector:@selector(webView:didFinishNavigation:)])
         [self.originalDelegate webView:webView didFinishNavigation:navigation];
     
@@ -58,9 +67,28 @@
     }
 }
 
-- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+- (void)webView:(WKWebView *)webView
+    didFailNavigation:(WKNavigation *)navigation
+    withError:(NSError *)error {
+    
     if ([self.originalDelegate respondsToSelector:@selector(webView:didFailNavigation:withError:)])
         [self.originalDelegate webView:webView didFailNavigation:navigation withError:error];
+}
+
+- (void)webView:(WKWebView *)webView
+    decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction
+    decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    if ([self.originalDelegate respondsToSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)])
+        [self.originalDelegate webView:webView decidePolicyForNavigationAction:navigationAction decisionHandler:decisionHandler];
+}
+
+- (void)webView:(WKWebView *)webView
+    decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse
+    decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    
+    if ([self.originalDelegate respondsToSelector:@selector(webView:decidePolicyForNavigationResponse:decisionHandler:)])
+        [self.originalDelegate webView:webView decidePolicyForNavigationResponse:navigationResponse decisionHandler:decisionHandler];
 }
 
 - (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView {
