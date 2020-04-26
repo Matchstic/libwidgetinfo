88fb465a15186f2548b94646dc57cbf20b4cee68
Matt Clarke <mclarke0805@gmail.com>
2020-04-26 13:04:24 +0100

    Add AGPLv3 notice to file headers

--- daemon/Connection/XENDBaseDaemonListener.m
+++ daemon/Connection/XENDBaseDaemonListener.m
@@ -1,10 +1,17 @@
-//
-//  XENDBaseDaemonListener.m
-//  Daemon
-//
-//  Created by Matt Clarke on 17/09/2019.
-//
-
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
 #import "XENDBaseDaemonListener.h"
 #import "../Data Providers/XENDBaseRemoteDataProvider.h"
 #import "../Data Providers/Media/XENDMediaRemoteDataProvider.h"
--- lib/Hooks/XENDHijackedWebViewDelegate.m
+++ lib/Hooks/XENDHijackedWebViewDelegate.m
@@ -1,9 +1,17 @@
-//
-//  XENDHijackedWebViewDelegate.m
-//  libwidgetdata
-//
-//  Created by Matt Clarke on 05/09/2019.
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
 
 #import "XENDHijackedWebViewDelegate.h"
 #import "../Internal/XENDWidgetManager.h"
@@ -26,9 +34,7 @@
     return self;
 }
 
-///////////////////////////////////////////////////////
-// WKNavigationDelegate
-///////////////////////////////////////////////////////
+#pragma mark WKNavigationDelegate
 
 - (void)webView:(WKWebView *)webView
     didStartProvisionalNavigation:(WKNavigation *)navigation {
