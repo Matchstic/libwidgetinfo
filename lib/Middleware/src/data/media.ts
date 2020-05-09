32fefafeb6701dfc38d565f13e90f06ecf402ada
Matt Clarke <mclarke0805@gmail.com>
2020-05-09 20:00:25 +0100

    Document media provider a bit

--- lib/Middleware/src/data/media.ts
+++ lib/Middleware/src/data/media.ts
@@ -28,17 +28,63 @@ export interface MediaLibraryArtist {
 }
 
 /**
- * @ignore
+ * This interface represents a media track, including music videos. Be aware that not all fields will
+ * always have data; not all apps report the same level of detail.
  */
 export interface MediaTrack {
+    /**
+     * The title of the track
+     */
     title: string;
+
+    /**
+     * The name of the album this track belongs to, if available
+     */
     album: string;
+
+    /**
+     * The artist this track was created by, if available
+     */
     artist: string;
-    artwork: string; // URL of the track artwork, loaded by the native side. May be an empty string, if so, no artwork
+
+    /**
+     * A URL to the track's artwork, intended to be directly set to the `src` attribute of an `img` tag.
+     *
+     * If it is an empty string (`""`), then there is no artwork available.
+     */
+    artwork: string;
+
+    /**
+     * The composer of the track, if available
+     */
     composer: string;
+
+    /**
+     * The genre of the track, if available
+     */
     genre: string;
+
+    /**
+     * The length of the track, measured in seconds.
+     *
+     * NOTE: This may be reported as 0 in some cases, if the playing application doesn't make this information available
+     *
+     * NOTE: This may also be reported as negative, such as when streaming media over AirPlay. This can be assumed to be representative of a "buffering" state.
+     */
     length: number;
-    number: number; // The track number on its corresponding album
+
+    /**
+     * The track number on its corresponding album
+     */
+    number: number;
+
+    /**
+     * The elapsed playback time, measured in seconds. This is only available on the currently playing track
+     *
+     * NOTE: This may be reported as 0 in some cases, if the playing application doesn't make this information available
+     *
+     * NOTE: This may also be reported as negative, such as when streaming media over AirPlay. This can be assumed to be representative of a "buffering" state.
+     */
     elapsed: number; // available only on the current track
 }
 
@@ -59,7 +105,16 @@ export interface MediaProperties {
 }
 
 /**
- * @ignore
+ * The Media provider brings together data about any currently playing media on the user's device.
+ *
+ * It provides access to everything available to the stock "Now Playing" widget of the Control Centre. This includes artwork, the "Up next" queue, and so forth.
+ *
+ * Also provided is a few utility functions, such as:
+ * - "Seeking" within the current track
+ * - Changing play/pause state
+ * - Skip forward or backwards between tracks
+ *
+ * You can register for an update timer, which notifies your code when the `elapsed` time of the current track changes.
  */
 export default class Media extends Base implements MediaProperties {
 
@@ -71,14 +126,45 @@ export default class Media extends Base implements MediaProperties {
     // MediaProperties stub implementation
     /////////////////////////////////////////////////////////
 
+    /**
+     * Specifies details about the currently playing track.
+     */
     nowPlaying: MediaTrack;
+
+    /**
+     * Provides a list of the next few tracks that will be played.
+     *
+     * This is only populated if the playing application supports it. Otherwise, it's an empty list.
+     *
+     * NOTE: Information is only available up to the next 15 tracks.
+     */
     queue: MediaTrack[];
 
+    /**
+     * Specifies whether the user has currently paused playback or not.
+     */
     isPlaying: boolean;
+
+    /**
+     * Specifies whether playback is actually possible right now.
+     *
+     * For example, this will be `false` when the user closes the currently playing app from the App Switcher.
+     */
     isStopped: boolean;
+
+    /**
+     * @ignore
+     */
     isShuffleEnabled: boolean;
 
+    /**
+     * @ignore
+     */
     volume: number;
+
+    /**
+     * Provides details about the application that is currently playing media
+     */
     nowPlayingApplication: ApplicationMetadata;
 
     // Replicate here for documentation purposes
