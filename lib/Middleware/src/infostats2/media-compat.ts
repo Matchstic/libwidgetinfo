c5fc2a0e37943438c3e6aaae66be487fddb9df5a
Matt Clarke <mclarke0805@gmail.com>
2020-05-11 01:49:45 +0100

    Hookup IS2 media compatibility

--- lib/Middleware/src/infostats2/media-compat.ts
+++ lib/Middleware/src/infostats2/media-compat.ts
@@ -23,28 +23,28 @@ export default class IS2Calendar {
         this._lookupMap['registerForTimeInformationWithIdentifier:andCallback:'] = (args: any[]) => {};
         this._lookupMap['unregisterForTimeInformationWithIdentifier:'] = (args: any[]) => {};
 
-        this._lookupMap['skipToNextTrack']              = () => { /* not implemented */ };
-        this._lookupMap['skipToPreviousTrack']          = () => { /* not implemented */ };
-        this._lookupMap['togglePlayPause']              = () => { /* not implemented */ };
-        this._lookupMap['play']                         = () => { /* not implemented */ };
-        this._lookupMap['pause']                        = () => { /* not implemented */ };
-        this._lookupMap['setVolume:withVolumeHUD: ']    = (args: any[]) => { /* not implemented */ };
+        this._lookupMap['skipToNextTrack']              = () => { this.provider.nextTrack(); };
+        this._lookupMap['skipToPreviousTrack']          = () => { this.provider.previousTrack(); };
+        this._lookupMap['togglePlayPause']              = () => { this.provider.togglePlayPause(); };
+        this._lookupMap['play']                         = () => { this.provider.togglePlayPause(); };
+        this._lookupMap['pause']                        = () => { this.provider.togglePlayPause(); };
+        this._lookupMap['setVolume:withVolumeHUD: ']    = (args: any[]) => { this.provider.setVolume(args[0] * 100); };
 
-        this._lookupMap['currentTrackTitle']            = () => { /* not implemented */ return ''; };
-        this._lookupMap['currentTrackArtist']           = () => { /* not implemented */ return ''; };
-        this._lookupMap['currentTrackAlbum']            = () => { /* not implemented */ return ''; };
-        this._lookupMap['currentTrackArtwork']          = () => { /* not implemented */ return ''; };
-        this._lookupMap['currentTrackArtworkBase64']    = () => { /* not implemented */ return ''; };
-        this._lookupMap['currentTrackLength']           = () => { /* not implemented */ return 0; };
-        this._lookupMap['elapsedTrackLength']           = () => { /* not implemented */ return 0; };
-        this._lookupMap['trackNumber']                  = () => { /* not implemented */ return 0; };
+        this._lookupMap['currentTrackTitle']            = () => { return this.provider.nowPlaying.title; };
+        this._lookupMap['currentTrackArtist']           = () => { return this.provider.nowPlaying.artist; };
+        this._lookupMap['currentTrackAlbum']            = () => { return this.provider.nowPlaying.album; };
+        this._lookupMap['currentTrackArtwork']          = () => { return this.provider.nowPlaying.artwork; };
+        this._lookupMap['currentTrackArtworkBase64']    = () => { return this.provider.nowPlaying.artwork; };
+        this._lookupMap['currentTrackLength']           = () => { return this.provider.nowPlaying.length; };
+        this._lookupMap['elapsedTrackLength']           = () => { return this.provider.nowPlaying.elapsed; };
+        this._lookupMap['trackNumber']                  = () => { return this.provider.nowPlaying.number; };
         this._lookupMap['totalTrackCount']              = () => { /* not implemented */ return 0; };
-        this._lookupMap['currentPlayingAppIdentifier']  = () => { /* not implemented */ return ''; };
-        this._lookupMap['shuffleEnabled']               = () => { /* not implemented */ return false; };
+        this._lookupMap['currentPlayingAppIdentifier']  = () => { return this.provider.nowPlayingApplication.identifier };
+        this._lookupMap['shuffleEnabled']               = () => { return this.provider.isShuffleEnabled; };
         this._lookupMap['iTunesRadioPlaying']           = () => { /* not implemented */ return false; };
-        this._lookupMap['isPlaying']                    = () => { /* not implemented */ return false; };
-        this._lookupMap['hasMedia']                     = () => { /* not implemented */ return false; };
-        this._lookupMap['getVolume ']                   = () => { /* not implemented */ return 0; };
+        this._lookupMap['isPlaying']                    = () => { return this.provider.isPlaying; };
+        this._lookupMap['hasMedia']                     = () => { return !this.provider.isStopped; };
+        this._lookupMap['getVolume ']                   = () => { return this.provider.volume / 100.0; };
     }
 
     public initialise(provider: Media) {
